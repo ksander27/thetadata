0c68e3c4c2bb77be0a4b090c67456d4ea05d2089
ksander27 <110091632+ksander27@users.noreply.github.com>
2023-02-13 22:22:16 +0700

    modification _get_end()

--- wrapper.py
+++ wrapper.py
@@ -265,7 +265,7 @@ class ThetaWrapper(ThetaClient):
         while not nyse.valid_days(start_date=end, end_date=end).size >0:
             end -= datetime.timedelta(days=1)
 
-        return end.fromisoformat(str(end))
+        return datetime.date.fromisoformat(str(end))
 
     def _validate_and_handle_error(self, ticker: Optional[str], start: str, req: str, end: Optional[str] = None, interval: Optional[int] = None, greek: Optional[int] = None) -> None:
             """
