98569e599bd792e65beee21455c8de08fcaf2aca
ksander27 <110091632+ksander27@users.noreply.github.com>
2023-02-22 16:12:26 +0100

    modification __init__.py

--- wrapper/__init__.py
+++ wrapper/__init__.py
@@ -1,4 +1,4 @@
-from .wrapper import MyWrapper
+from .wrapper import MyWrapper,RootOrExpirationError
 from .option import Option
 from .stock import Stock
 from .utils import ResponseFormatError,IVLError
