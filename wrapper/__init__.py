f4dc1f877abf60f30ad710d2a0d0226340367723
ksander27 <110091632+ksander27@users.noreply.github.com>
2023-02-25 16:53:12 +0800

    version 1

--- wrapper/__init__.py
+++ wrapper/__init__.py
@@ -1,4 +1,4 @@
 from .wrapper import MyWrapper,RootOrExpirationError
-from .option import Option
+from .option import Option,OptionError,RightError,StrikeError
 from .stock import Stock
 from .utils import ResponseFormatError,IVLError
--- wrapper/stock.py
+++ wrapper/stock.py
@@ -8,7 +8,60 @@ class Stock(MyWrapper):
         super().__init__()
         self.sec_type = "stock"
         self.root = root
+    
+    # Call endpoint helpers
+    def _get_list_dates(self):
+        self.call_type = "list"
+        
+        self.url = f"{self.base_url}/{self.call_type}/dates/{self.sec_type}/{self.req_type}"
+        self.params = {"root":self.root}
+        return self._get_data()
+    
+    def _get_hist(self,start_date,end_date,ivl):
+        self.call_type = "hist"
+        
+        _start_date = _format_date(start_date)
+        _end_date = _format_date(end_date)
+        _ivl = _format_ivl(ivl)
+        
+        self.url = f"{self.base_url}/{self.call_type}/{self.sec_type}/{self.req_type}"
+        if _isDateRangeValid(_start_date,_end_date):
+            self.params = {
+                "start_date":_start_date
+                ,"end_date":_end_date
+                ,"root":self.root
+                ,"ivl": _ivl
+            }
+            return self._get_data()
+    
+    def _get_at_time(self,start_date,end_date,ivl):
+        self.call_type = "at_time"
+        
+        _start_date = _format_date(start_date)
+        _end_date = _format_date(end_date)
+        _ivl = _format_ivl(ivl)
+        
+    
+        self.url = f"{self.base_url}/{self.call_type}/{self.sec_type}/{self.req_type}"
 
+        if _isDateRangeValid(_start_date,_end_date):
+            self.params = {
+                "start_date":_start_date
+                ,"end_date":_end_date
+                ,"root":self.root
+                ,"ivl": _ivl
+            }
+            return self._get_data()
+    
+    # List endpoints
+    def get_list_expirations_trade(self) -> List[str]:
+        self.req_type = "trade"
+        return self._get_list_dates()
+    
+    def get_list_expirations_quote(self) -> List[str]:
+        self.req_type = "quote"
+        return self._get_list_dates()
+    
     def get_list_roots(self) -> List[str]:
         """
         Retrieves all roots for the specified security.
@@ -28,35 +81,36 @@ class Stock(MyWrapper):
 
         return self._get_data()
     
-    def _get_hist(self,start_date,end_date,ivl):
-        self.call_type = "hist"
-        
-        _start_date = _format_date(start_date)
-        _end_date = _format_date(end_date)
-        _ivl = _format_ivl(ivl)
-        
-        self.url = f"{self.base_url}/{self.call_type}/{self.sec_type}/{self.req_type}"
-        self.params = {
-            "start_date":_start_date
-            ,"end_date":_end_date
-            ,"root":self.root
-            ,"ivl": _ivl
-        }
-        return self._get_data()
+    # hist endpoints
+    def get_hist_quote(self,start_date,end_date,ivl):
+        self.req_type = "quote"
+        return self._get_hist(start_date,end_date,ivl)
+                
+    def get_hist_trade(self,start_date,end_date,ivl):
+        self.req_type = "trade"
+        return self._get_hist(start_date,end_date,ivl)
     
-    def _get_at_time(self,start_date,end_date,ivl):
-        self.call_type = "at_time"
-        
-        _start_date = _format_date(start_date)
-        _end_date = _format_date(end_date)
-        _ivl = _format_ivl(ivl)
-        
+    def get_hist_ohlc(self,start_date,end_date,ivl):
+        self.req_type = "ohlc"
+        return self._get_hist(start_date,end_date,ivl)
     
-        self.url = f"{self.base_url}/{self.call_type}/{self.sec_type}/{self.req_type}"
-        self.params = {
-            "start_date":_start_date
-            ,"end_date":_end_date
-            ,"root":self.root
-            ,"ivl": _ivl
-        }
-        return self._get_data()
+    def get_hist_eod(self,start_date,end_date,ivl):
+        self.req_type = "eod"
+        return self._get_hist(start_date,end_date,ivl)
+    
+    # at_times endpoint
+    def get_at_time_quote(self,start_date,end_date,ivl):
+        self.req_type = "quote"
+        return self._get_at_time(start_date,end_date,ivl)
+                
+    def get_at_time_trade(self,start_date,end_date,ivl):
+        self.req_type = "trade"
+        return self._get_at_time(start_date,end_date,ivl)
+    
+    def get_at_time_ohlc(self,start_date,end_date,ivl):
+        self.req_type = "ohlc"
+        return self._get_at_time(start_date,end_date,ivl)
+    
+    def get_at_time_eod(self,start_date,end_date,ivl):
+        self.req_type = "eod"
+        return self._get_at_time(start_date,end_date,ivl)
