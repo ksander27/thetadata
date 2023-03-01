1c34a698083af6eb530a1e746069e7043fe5ad08
ksander27 <110091632+ksander27@users.noreply.github.com>
2023-03-01 23:24:56 +0800

    modif

--- wrapper/async_fetch.py
+++ wrapper/async_fetch.py
@@ -18,10 +18,10 @@ async def _fetch_task(contract,session,max_retry):
 
                     if contract._parse_header():
                         data = contract._parse_response()
-                        print(f"[+] Fetched data for contract - {contract.__str__()} - {contract.params}")
+                        #print(f"[+] Fetched data for contract - {contract.__str__()} - {contract.params}")
                         return {"data":data,"url":contract.url,"params":contract.params}
         except NoDataForContract:
-            print(f"[+] No data for contract - {contract.__str__()} - {contract.params}")
+            #print(f"[+] No data for contract - {contract.__str__()} - {contract.params}")
             return {"data":None,"url":None,"params":None}
         except aiohttp.ClientError:
             retry_count += 1
@@ -41,7 +41,7 @@ async def fetch_all_contracts(contracts,timeout=20,max_retry=2):
     Fetch data for all contracts asynchronously.
     """
     async with aiohttp.ClientSession() as session:
-        for retry in range(3):
+        for retry in range(max_retry):
             try:
                 data = await asyncio.wait_for(_gather_tasks(contracts, session,max_retry), timeout=timeout)
                 return data
@@ -49,6 +49,6 @@ async def fetch_all_contracts(contracts,timeout=20,max_retry=2):
                 if retry == max_retry:
                     print("Timeout: maximum number of retries reached.")
                     raise
-                print(f"Timeout: retrying ({retry+1}/2)...")
+                print(f"Timeout: retrying ({retry+1}/{max_retry})...")
 
 
