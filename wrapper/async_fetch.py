74742b0911098a116acaf630a805c4a3a4585f5e
ksander27 <110091632+ksander27@users.noreply.github.com>
2023-03-03 16:19:57 +0700

    _fetch_task

--- wrapper/async_fetch.py
+++ wrapper/async_fetch.py
@@ -6,29 +6,27 @@ from .wrapper import NoDataForContract
 # Function to be added in _get_data and will be played if _async = True
 async def _fetch_task(contract,session,max_retry):
     retry_count = 0
-    while retry_count < max_retry:
-        try:
-            async with session.get(contract.url,params=contract.params) as r:
-                if r.status !=200:
-                    r.raise_for_status()
-                else:
-                    _ = await r.json()
-                    contract.header = _.get("header")
-                    contract.response = _.get("response")
 
-                    if contract._parse_header():
-                        data = contract._parse_response()
-                        print(f"[+] Fetched data for contract - {contract.__str__()} - {contract.params}")
-                        return {"data":data,"url":contract.url,"params":contract.params}
-        except NoDataForContract:
-            print(f"[+] No data for contract - {contract.__str__()} - {contract.params}")
-            return {"data":None,"url":None,"params":None}
-        except aiohttp.ClientError:
-            retry_count += 1
-        except asyncio.CancelledError:
-            print(f"[+] Task cancelled for contract - {contract.__str__()} - {contract.params}")
-            return {"data": None, "url": None, "params": None}
-    raise asyncio.TimeoutError(f"Timeout for {contract.url} after {retry_count} retries")
+    try:
+        async with session.get(contract.url,params=contract.params) as r:
+            if r.status !=200:
+                r.raise_for_status()
+            else:
+                _ = await r.json()
+                contract.header = _.get("header")
+                contract.response = _.get("response")
+
+                if contract._parse_header():
+                    data = contract._parse_response()
+                    print(f"[+] Fetched data for contract - {contract.__str__()} - {contract.params}")
+                    return {"data":data,"url":contract.url,"params":contract.params}
+    except NoDataForContract:
+        print(f"[+] No data for contract - {contract.__str__()} - {contract.params}")
+        return {"data":None,"url":None,"params":None}
+    except aiohttp.ClientError:
+        retry_count += 1
+
+
 
         
 async def _gather_tasks(contracts,session,max_retry):
