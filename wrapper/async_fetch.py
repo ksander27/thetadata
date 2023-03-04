f575a8905c82d15f59491ff6ab0e560035614922
ksander27 <110091632+ksander27@users.noreply.github.com>
2023-03-04 23:02:04 +0700

    commit

--- wrapper/async_fetch.py
+++ wrapper/async_fetch.py
@@ -55,7 +55,7 @@ async def fetch_all_contracts(contracts_in_exp, batch_size=32, TIMEOUT=20, MAX_R
             i += 1
             print(f"[+] Timed out {i}/{MAX_RETRY} .. going to sleep")
             await asyncio.sleep(SLEEP)
-        except TaskError:
-            break
+        #except TaskError:
+        #    break
     print(f"[+] Max retries reached. Giving up")
     return data
