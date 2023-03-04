7239c1a1c81b7ccc0905978d6fa4620f5c8f160f
ksander27 <110091632+ksander27@users.noreply.github.com>
2023-03-04 09:08:36 +0700

    async

--- wrapper/async_fetch.py
+++ wrapper/async_fetch.py
@@ -1,30 +1,8 @@
 import asyncio 
 import aiohttp
 import random
-import time
 from .wrapper import NoDataForContract
 
-class TaskTimeOut(Exception):
-    pass
-
-# Function to be added in _get_data and will be played if _async = True
-# async def _fetch_task(contract, session):
-#     try:
-#         async with session.get(contract.url, params=contract.params) as r:
-#             if r.status != 200:
-#                 r.raise_for_status()
-#             else:
-#                 _ = await r.json()
-#                 contract.header = _.get("header")
-#                 contract.response = _.get("response")
-
-#                 if contract._parse_header():
-#                     data = contract._parse_response()
-#                     print(f"[+] Fetched data for contract - {contract.__str__()} - {contract.params}")
-#                     return {"data": data, "url": contract.url, "params": contract.params}
-#     except NoDataForContract:
-#         print(f"[+] No data data for contract - {contract.__str__()} - {contract.params}")
-#         return {"data": None, "url": None, "params": None}
 
 async def _fetch_task(contract, session, TIMEOUT,MAX_RETRY,SLEEP):
     for i in range(MAX_RETRY):
@@ -36,7 +14,6 @@ async def _fetch_task(contract, session, TIMEOUT,MAX_RETRY,SLEEP):
                     _ = await r.json()
                     contract.header = _.get("header")
                     contract.response = _.get("response")
-                    await asyncio.sleep(SLEEP * (2 ** i) + random.uniform(0, 1))
 
                     if contract._parse_header():
                         data = contract._parse_response()
@@ -49,9 +26,8 @@ async def _fetch_task(contract, session, TIMEOUT,MAX_RETRY,SLEEP):
         except asyncio.TimeoutError:
             print(f"[+] Timed out for contract - {contract.__str__()} - {contract.params}")
             await asyncio.sleep(SLEEP * (2 ** i) + random.uniform(0, 1))
-            
+
     print(f"Failed to fetch data for contract - {contract.__str__()} - {contract.params}")
-    await asyncio.sleep(SLEEP * (2 ** i) + random.uniform(0, 1))
     return {"data": None, "url": contract.url, "params": contract.params}
 
 
