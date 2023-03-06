72aa5621ecafaea28424784e6c6ad6da7a4ce0c4
ksander27 <110091632+ksander27@users.noreply.github.com>
2023-03-06 19:30:43 +0800

    timeout

--- wrapper/async_fetch.py
+++ wrapper/async_fetch.py
@@ -59,6 +59,7 @@ async def fetch_all_contracts(contracts_in_exp, batch_size=32, TIMEOUT=5, MAX_RE
             break
         except asyncio.TimeoutError:
             i -= 1
+            TIMEOUT += batch_size
             print(f"[+] Timed out {MAX_RETRY-i}/{MAX_RETRY} .. going to sleep")
             await asyncio.sleep(SLEEP)
     else:
