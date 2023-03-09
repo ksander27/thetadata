2c2f4741a7fc3d2c01b473dd36bbf41351df7bdd
ksander27 <110091632+ksander27@users.noreply.github.com>
2023-03-09 13:05:54 +0800

    example

--- wrapper/example/example_async_fetching.py
+++ wrapper/example/example_async_fetching.py
@@ -1,14 +1,17 @@
-from wrapper import Option,fetch_all_contracts
+# How to retrieve open interest
+
+from wrapper import Option,AsyncFetcher
 
 import pandas as pd 
 import asyncio
 
-def get_option(args,strike,right,params):
-    args["strike"] = int(int(strike.get("strikes"))/1000)
+def get_option(args,strike,right,params_method,method):
+    args["strike"] = strike
     args["right"] = right
     args["_async"] = True
+    params = {"method":method,"params":params_method}
     opt = Option(**args)
-    opt.get_hist_open_interest(**params)
+    opt._get_method(**params)
     return opt
 
 def get_df_tmp(contract):
@@ -30,21 +33,31 @@ if __name__=='__main__':
         ,"exp":"20230317"
     }
 
-    params = {
+    params_method = {
         "start_date":"20230101"
         ,"end_date":"20230201"
     }
 
+    fetcher_params = {
+        "batch_size":42
+        ,"timeout":42
+        ,"sleep":4
+        ,"max_retry":2
+    }
+
+    method = "get_hist_open_interest"
+
     # Init get strikes
     option = Option(**args)
-    strikes = option.get_list_strikes()
+    strikes = [int(strike.get("strikes"))/1000 for strike in option.get_list_strikes()]
 
     # Get options with url and params
-    options = [get_option(args,strike,right,params) for strike in strikes for right in ["call","put"]]
+    options = [get_option(args,strike,right,params_method) for strike in strikes for right in ["call","put"]]
     print(f"[+] About to fetch {len(options)} options...")
 
     # Fetching responses
-    fetched_contracts = asyncio.run(fetch_all_contracts(options))
+    fetcher = AsyncFetcher(**fetcher_params)
+    fetched_contracts = asyncio.run(fetcher.fetch_all_contracts(options))
     print(f"[+] Fetched {len(fetched_contracts)} responses.")
 
     # Format data 
@@ -54,7 +67,7 @@ if __name__=='__main__':
 
     # Save 
     root,exp = args.get("root"),args.get("exp")
-    _start,_end = params.get("start_date"),params.get("end_date")
+    _start,_end = params_method.get("start_date"),params_method.get("end_date")
     _filename = f"./{root}_{exp}_{_start}_{_end}"
     df.to_csv(_filename,index=False)
     print(f"[+] Save {_filename} with shape {df.shape}")
