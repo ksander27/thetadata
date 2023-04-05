cb944834e7219c232be3a419b1924cfd655b6545
ksander27 <110091632+ksander27@users.noreply.github.com>
2023-04-05 09:10:59 +0800

    manager

--- tools/manager.py
+++ tools/manager.py
@@ -252,7 +252,7 @@ class ExpiryManager(AppManager):
                                                                 for right in ["call","put"]])
             
             method, key_params = "get_list_dates_implied_volatility",[]
-            print(df_batches)
+            print(df_batches.head())
             downloader = AsyncDownloaderOption(batches=df_batches,method=method,key_params=key_params
                             ,batch_size=self.BATCH_SIZE,timeout=self.TIMEOUT,max_retry=self.MAX_RETRY,sleep=self.SLEEP)
             df_dates = downloader.async_download_contracts()
